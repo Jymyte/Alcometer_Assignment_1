a73611e4206afdda3bd832d616ea760a660039a0
jymyte <jyrimy@gmail.com>
2022-01-26 15:31:17 +0200

    Add functionality to exchange-rates-a2 app.
    Now working.

--- exchange-rates-a2/src/App.js
+++ exchange-rates-a2/src/App.js
@@ -10,6 +10,8 @@ const API_ADDRESS = API_URL + API_KEY
 //useffectillä haku
 function App() {
   const [rates, setRates] = useState([]);
+  const [euros, setEuros] = useState(0);
+  const [conversion, setConversion] = useState(0);
 
   /* const promise = axios.get(API_ADDRESS)
   console.log(promise); */
@@ -24,9 +26,20 @@ function App() {
 
   console.log(rates);
   
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const temp = euros * rates.GBP;
+    setConversion(temp)
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <input type="double" onChange={(e) => setEuros(e.target.value)}></input>
+        <label> x {rates.GBP}</label>
+        <button type="submit">Calculate</button>
+      </form>
+      <label>{conversion} GBP</label>
     </div>
   );
 }
