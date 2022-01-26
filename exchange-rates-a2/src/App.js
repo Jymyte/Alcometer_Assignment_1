a1db262ca4848fdd1c296ca5d8a104e737bb2ee7
jymyte <jyrimy@gmail.com>
2022-01-26 17:38:16 +0200

    Cleaned up the code of exchange-rates-a2 a bit.

--- exchange-rates-a2/src/App.js
+++ exchange-rates-a2/src/App.js
@@ -1,20 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
-
-
 const API_URL = "http://api.exchangeratesapi.io/v1/latest?access_key=";
 const API_KEY = process.env.REACT_APP_EXCHANGE_RATES_API_KEY
 
 const API_ADDRESS = API_URL + API_KEY
-//useffectillä haku
+
 function App() {
   const [rates, setRates] = useState([]);
   const [euros, setEuros] = useState(0);
   const [conversion, setConversion] = useState(0);
-
-  /* const promise = axios.get(API_ADDRESS)
-  console.log(promise); */
 
   useEffect(() => {
     axios.get(API_ADDRESS)
